02d290cab80a386f09fd44c1805337c6b42c01da
Sandi Gec <sandi.gec@gmail.com>
2018-03-21 11:03:40 +0100

    Update ASAPFusekiUtils.java

--- src/main/java/org/ul/asap/webapp/util/ASAPFusekiUtils.java
+++ src/main/java/org/ul/asap/webapp/util/ASAPFusekiUtils.java
@@ -1,24 +1,21 @@
 package org.ul.asap.webapp.util;
 
-import org.apache.jena.query.*;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.ResIterator;
-import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.reasoner.ReasonerRegistry;
 import org.apache.jena.sparql.core.ResultBinding;
 import org.apache.jena.sparql.core.Var;
-import org.apache.jena.update.UpdateExecutionFactory;
-import org.apache.jena.update.UpdateFactory;
-import org.apache.jena.update.UpdateProcessor;
-import org.apache.jena.update.UpdateRequest;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.ReasonerVocabulary;
-import org.apache.jena.vocabulary.VCARD;
-import org.joda.time.DateTime;
 import org.ul.asap.webapp.entry.*;
 import org.ul.common.monitoring.MonitoringMetric;
 import org.ul.common.rest.IService;
+import org.ul.switch.webapp.entry.MyEntry;
+import org.ul.switch.webapp.entry.ResultObj;
 
 import java.io.*;
 import java.net.URL;
@@ -28,7 +25,7 @@ public class ASAPFusekiUtils {
 
     private static final String XSD_PREFIX = "xsd:<http://www.w3.org/2001/XMLSchema#>";
     private static final String RDF_PREFIX = "rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>";
-    public static String KB_PREFIX_SHORT = "http://www.semanticweb.org/project-entice/ontologies/2015/7/knowledgebase#";
+    public static String KB_PREFIX_SHORT = "http://www.semanticweb.org/project-switch/ontologies/2015/7/knowledgebase#";
     private static String KB_PREFIX = ":<http://www.switch-project.eu/ontology/switch-component#>";
     private static String OWL_PREFIX = "owl:<http://www.w3.org/2002/07/owl#>";
 
@@ -69,6 +66,14 @@ public class ASAPFusekiUtils {
                         "        :ClusterCredentials_CertificateAuthority     \"%s\" ;" +
                         "}", UUID.randomUUID().toString(), cc.getMasterURL(), cc.getClientPublicCredentials(), cc.getClientPrivateCredentials(), cc.getCertificateAuthority());
             }
+            // CREATE SWITCH CONFIGURATION SERVICE
+            else if (obj instanceof SCS) {
+                SCS scs = (SCS) obj;
+
+                return String.format("PREFIX " + KB_PREFIX + " PREFIX " + OWL_PREFIX + " INSERT DATA {" +
+                        ":%s  a :SCS, owl:NamedIndividual ;" +
+                        "}", scs.getId());
+            }
             // CREATE MONITORING METRIC
             else if (obj instanceof MonitoringMetric) {
                 MonitoringMetric mm = (MonitoringMetric) obj;
@@ -88,6 +93,21 @@ public class ASAPFusekiUtils {
                                 "}", UUID.randomUUID().toString(), mm.getCollectingInterval(), mm.getDataType(),
                         mm.getGroup(), mm.getLowerLimit(), mm.getName(), mm.getThreshold(), mm.getUnit(),
                         mm.getUpperLimit(), mm.getRelationalOperator());
+            }
+            // CREATE ALARM TRIGGER
+            else if (obj instanceof AlarmTrigger) {
+                AlarmTrigger at = (AlarmTrigger) obj;
+
+                return String.format(Locale.US, "PREFIX " + KB_PREFIX + " PREFIX " + OWL_PREFIX + " PREFIX " +
+                                OWL_PREFIX + "PREFIX " + XSD_PREFIX + " INSERT DATA {" +
+                                ":%s  a :AlarmTrigger, owl:NamedIndividual ;" +
+                                "        :AlarmTrigger_Label     \"%s\" ;\n" +
+                                "        :AlarmTrigger_Time  \"%s\" ;\n" +
+                                "        :AlarmTrigger_ClassID     \"%s\" ;\n" +
+                                "        :AlarmTrigger_Value     \"%s\" ;\n" +
+                                "        :AlarmTrigger_IsCritical     \"%s\" ;\n" +
+                                "}", at.getId(), at.getLabel(), at.getTime().getMillis(), at.getClassID(),
+                        at.getValue(), at.isCritical());
             } else {
                 throw new InvalidObjectException("The selected object type is not currently supported!");
             }
@@ -111,25 +131,6 @@ public class ASAPFusekiUtils {
         return objectivesStr;
     }
 
-    @Deprecated
-    public static String getPassword(String username) {
-        return "prefix knowledgebase: <http://www.semanticweb.org/project-entice/ontologies/2015/7/knowledgebase#>\n" +
-                "\n" +
-                "SELECT ?pass\n" +
-                "WHERE { ?s a knowledgebase:User ; knowledgebase:User_UserName \"" + username + "\" ; " +
-                "knowledgebase:User_Password ?pass }\n" +
-                "LIMIT 25\n";
-    }
-
-
-    public static String performUserLogin(String username, String password) {
-        return "prefix knowledgebase: <http://www.semanticweb.org/project-entice/ontologies/2015/7/knowledgebase#>\n" +
-                "\n" +
-                "SELECT ?s ?privilege \n" +
-                "WHERE { ?s a knowledgebase:User ; knowledgebase:User_UserName \"" + username + "\" ; " +
-                "knowledgebase:User_Password \"" + password + "\" ; knowledgebase:User_Privilege ?privilege }\n" +
-                "LIMIT 25\n";
-    }
 
     public static String getAllEntitiesQuery(String entityClass, String... queryFilterCondition) {
         if (entityClass.equals("ClusterCredentials") && queryFilterCondition.length > 0) {
@@ -143,68 +144,17 @@ public class ASAPFusekiUtils {
 //                    "LIMIT 200";
     }
 
-    public static String getAllEntitiesCount(String entityClass, String... queryFilterCondition) {
-        if (entityClass.equals("DiskImage") && queryFilterCondition.length > 0)
-            return "prefix knowledgebase: <http://www.semanticweb" +
-                    ".org/project-entice/ontologies/2015/7/knowledgebase#>\n" +
-                    "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
-                    "\n" +
-                    "SELECT (COUNT(DISTINCT ?s) AS ?count)\n" +
-                    "WHERE { ?s a knowledgebase:" + entityClass + " ; knowledgebase:DiskImage_hasOwner " +
-                    "\"" + queryFilterCondition[0] + "\"^^xsd:anyURI }";
-        else
-            return "prefix knowledgebase: <http://www.semanticweb" +
-                    ".org/project-entice/ontologies/2015/7/knowledgebase#>\n" +
-                    "SELECT (COUNT(DISTINCT ?s) AS ?count)\n" +
-                    "WHERE { ?s a knowledgebase:" + entityClass + "}";
-    }
-
-    public static String getEntityQuery(String entityClass, String... postfixCondition) {
-        if (postfixCondition.length > 1)
-            return "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
-                    "prefix knowledgebase: <http://www.semanticweb" +
-                    ".org/project-entice/ontologies/2015/7/knowledgebase#>\n" +
-                    "\n" +
-                    "SELECT ?s ?p ?o\n" +
-                    "WHERE { ?s a knowledgebase:" + entityClass + " " + postfixCondition[0] + " }\n" +
-                    postfixCondition[1];
-
-        return "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
-                "prefix knowledgebase: <http://www.semanticweb" +
-                ".org/project-entice/ontologies/2015/7/knowledgebase#>\n" +
-                "\n" +
-                "SELECT ?s ?p ?o\n" +
-                "WHERE { ?s a knowledgebase:" + entityClass + " ; ?p ?o }\n" + postfixCondition[0];
-    }
-
-    public static String getIdBasedEntitiesQuery(String clazz, String id) {
-        if (clazz.equals("Fragment")) return "prefix knowledgebase: <http://www.semanticweb" +
-                ".org/project-entice/ontologies/2015/7/knowledgebase#>\n" +
-                "SELECT ?s\n" +
-                "WHERE { ?s a knowledgebase:Fragment ; knowledgebase:Fragment_hasReferenceImage ?o , " +
-                "knowledgebase:" + id + " }\n";
-//                "LIMIT 200";
-        else throw new UnsupportedOperationException("not implemented for this class");
-    }
-
-    public static String getHistoryDataIDs(String fragmentId) {
-        return "prefix knowledgebase: <http://www.semanticweb.org/project-entice/ontologies/2015/7/knowledgebase#>\n" +
-                "prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#>\n" +
-                "prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-                "\n" +
-                "select ?o\n" +
-                "where { knowledgebase:" + fragmentId + " knowledgebase:hasHistoryData ?o\n" +
-                "}";
-    }
-
-    public static String getDiskImageIDs() {
-        return "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
-                "prefix knowledgebase: <http://www.semanticweb.org/project-entice/ontologies/2015/7/knowledgebase#>\n" +
-                "\n" +
-                "#SELECT count(*)\n" +
-                "SELECT DISTINCT ?s\n" +
-                "WHERE { ?s a knowledgebase:DiskImage . ?s a owl:NamedIndividual , ?o }\n" +
-                "#FILTER(?o IN knowledgebase:DiskImage )";
+    public static String getAllUploadedImages(Boolean optimizedOnly) {
+//        return "SELECT DISTINCT ?s\n" +
+//                "WHERE { ?s ?val  ?obj  FILTER regex(str(?s), \"http://example/\")   }\n" +
+//                "LIMIT 100";
+        if (optimizedOnly == null) return "SELECT DISTINCT ?s\n" +
+                "WHERE { ?s ?p  ?o  FILTER regex(str(?s), \"http://example/\")   }\n" +
+                "LIMIT 25";
+        else return "SELECT DISTINCT ?s\n" +
+                "WHERE { ?s <http://purl.org/dc/elements/1.1/optimize> \"" + (optimizedOnly ? "true" : "false") + "\"" +
+                " . FILTER regex(str(?s), \"http://example/\")  }\n" +
+                "LIMIT 25";
     }
 
     public static String getAllAttributesMatchingSubjectID(String id) {
@@ -244,8 +194,6 @@ public class ASAPFusekiUtils {
             System.out.println(" " + ri.next());
         }
     }
-
-    public static final String myNamespacePrefix = "http://entice-project/jernej/test/";
 
     public static List<ResultObj> getResultObjectListFromResultSet(ResultSet results) {
         List<ResultObj> resultObjs = new ArrayList<ResultObj>();
@@ -291,10 +239,24 @@ public class ASAPFusekiUtils {
         return resultObjs;
     }
 
+    public static int getCountResult(ResultSet results) {
+        // For each solution in the result set
+        while (results.hasNext()) {
+            QuerySolution qs = results.next();
+            Iterator<Var> varIter = ((ResultBinding) qs).getBinding().vars();
+            while (varIter.hasNext()) {
+                Var var = varIter.next();
+                if (var.getVarName().equals("count"))
+                    return (int) ((ResultBinding) qs).getBinding().get(var).getLiteralValue();
+            }
+        }
+        return 0;
+    }
+
     public static <T extends MyEntry> List<T> getAllEntityAttributes(Class<T> clazz, String ontologyURL, String... conditions) {
         String selectQuery = ASAPFusekiUtils.getAllEntitiesQuery(clazz.getSimpleName(), conditions);
         QueryExecution qe = QueryExecutionFactory.sparqlService(ontologyURL, selectQuery);
-//        QueryExecution qe = QueryExecutionFactory.sparqlService("http://localhost:3030/entice/query", selectQuery);
+//        QueryExecution qe = QueryExecutionFactory.sparqlService("http://localhost:3030/switch/query", selectQuery);
         ResultSet results = qe.execSelect();
         List<ResultObj> resultObjs = ASAPFusekiUtils.getResultObjectListFromResultSet(results);
 
@@ -329,8 +291,9 @@ public class ASAPFusekiUtils {
         return list;
     }
 
-    public static List<MonitoringMetric> getAllEntityAttributes(Class<MonitoringMetric> monitoringMetricClass, String property) {
-        //TODO
+
+
+    public static Map<String, String> getTokenContent(String property, String token, List<String> keys) {
         return null;
     }
 }
